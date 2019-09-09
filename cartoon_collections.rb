323f43c8b5d1cb719d2c32e91b8afdb7e74ad28a
bogomilmihaylov <bogomilmihaylov@live.com>
2019-09-09 11:52:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,6 +4,7 @@ array.each_with_index {|name, index| puts "#{index + 1}.#{name} "}
 end
 
 
+
 def summon_captain_planet(array)
   planeteer_calls = [ ]
   
@@ -13,12 +14,13 @@ def summon_captain_planet(array)
 planeteer_calls  
 end
 
+
+
 def long_planeteer_calls(array)
   
   array.any? do |word| 
   word.length > 4
   end
-
 end
 
 def find_the_cheese(array)
